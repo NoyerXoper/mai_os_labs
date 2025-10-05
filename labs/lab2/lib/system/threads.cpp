a4ca2321ccd67ef4b0d83198131a141e27a6c152
NoyerXoper <chudov_2006@mail.ru>
2025-10-05 12:52:05 +0300

    unix thread api

---
+++ labs/lab2/lib/system/threads.cpp
@@ -0,0 +1,42 @@
+#include <pthread.h>
+#include <utility>
+
+#include "threads.hpp"
+
+
+namespace os {
+struct ThreadInfo {
+    pthread_t thread;
+};
+
+Thread::Thread(void_func func): function_(func) {
+    handle_ = new ThreadInfo();
+}
+
+Thread::Thread(Thread&& other) noexcept: function_(other.function_), handle_(other.handle_) {
+    function_ = nullptr;
+    other.handle_ = nullptr;
+}
+
+Thread& Thread::operator=(Thread&& other) noexcept {
+    if (this == &other) {
+        return *this;
+    }
+    Thread moved = std::move(other);
+    this->Swap(moved);
+    return *this;
+}
+
+bool Thread::Run(void* data) {
+    return pthread_create(&(handle_->thread), NULL, function_, data) == 0;
+}
+
+void Thread::Join() {
+    pthread_join(handle_->thread, NULL);
+}
+
+void Thread::Swap(Thread& other) {
+    std::swap(function_, other.function_);
+    std::swap(handle_, other.handle_);
+}
+}
